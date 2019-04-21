ef91dc5e0b3f00afde4a15395db67c423b403b20
espunosav <4576555+espunosav@users.noreply.github.com>
2019-04-20 21:21:29 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-/*var dog_whine = new Audio("/Audio/Dog_Whining.wav");
+var dog_whine = new Audio("/Audio/Dog_Whining.wav");
 function myAudioFunction(audio_file) {
     if(audio_file == "Dog_whine"){
         dog_whine.play();
     }
-};*/
+};
 
-function play() {
-    var audio = document.getElementById("audio");
-    audio.play();
-}
+// function play() {
+//     var audio = document.getElementById("audio");
+//     audio.play();
+// }
