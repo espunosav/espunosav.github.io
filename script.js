9664a85aedd38dddda3fa82738bd8d41df00b087
espunosav <4576555+espunosav@users.noreply.github.com>
2019-04-20 20:32:05 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var dog_whine = new Audio('Dog_Whining.wav');
+var dog_whine = new Audio("/Audio/Dog_Whining.wav");
 function myAudioFunction(audio_file) {
     if(audio_file == "Dog_whine"){
         dog_whine.play();
     }
-}
+};
