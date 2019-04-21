be200b1c37201b134e91a86e007fd4a5c391c448
espunosav <4576555+espunosav@users.noreply.github.com>
2019-04-20 22:01:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// var dog_whine = new Audio("/Audio/Dog_Whining.wav");
-// function myAudioFunction(audio_file) {
-//     if(audio_file == "Dog_whine"){
-//         dog_whine.play();
-//     }
-// };
-
 function play_single_sound() {
     var playPromise = document.getElementById('dog').play();
     if (playPromise !== undefined) {
