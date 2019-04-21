69276986b054effbdd710730923f6074d7c7d607
espunosav <4576555+espunosav@users.noreply.github.com>
2019-04-20 21:56:27 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-var dog_whine = new Audio("/Audio/Dog_Whining.wav");
-function myAudioFunction(audio_file) {
-    if(audio_file == "Dog_whine"){
-        dog_whine.play();
-    }
-};
+// var dog_whine = new Audio("/Audio/Dog_Whining.wav");
+// function myAudioFunction(audio_file) {
+//     if(audio_file == "Dog_whine"){
+//         dog_whine.play();
+//     }
+// };
 
-// function play() {
-//     var audio = document.getElementById("audio");
-//     audio.play();
-// }
+function play_single_sound() {
+    var playPromise = document.getElementById('dog').play();
+    if (playPromise !== undefined) {
+        playPromise.then(_ => {
+          // Automatic playback started!
+          // Show playing UI.
+        })
+        .catch(error => {
+          // Auto-play was prevented
+          // Show paused UI.
+        });
+    }
+}
