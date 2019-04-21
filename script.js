536c7de1f0fb62c97d8112714c00a42c40ba305d
espunosav <4576555+espunosav@users.noreply.github.com>
2019-04-20 22:34:22 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-function play_single_sound() {
-    var playPromise = document.getElementById('dog').play();
-    if (playPromise !== undefined) {
-        playPromise.then(_ => {
-          // Automatic playback started!
-          // Show playing UI.
-        })
-        .catch(error => {
-          // Auto-play was prevented
-          // Show paused UI.
-        });
-    }
-}
+
